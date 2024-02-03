14d07d48503b6dfb0951b37274af743d824e607d
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-02-02 21:30:26 -0500

    creando el mvc en nuestr api de la clase 3 con clases

---
+++ node-midu/clase-4/app-cors.js
@@ -0,0 +1,111 @@
+import express, { json } from 'express'
+import movies, { filter, push, findIndex, splice, find } from './movies.json'
+// usando la libreria de cors
+import cors from 'cors'
+import { randomUUID } from 'node:crypto'
+import { validateMovie, validatePartialMovie } from './schemas/movies'
+
+const app = express()
+app.use(json())
+app.use(cors({
+  origin: (origin, callback) => {
+    const ACCEPTED_ORIGINS = [
+      'http://localhost:8080',
+      'http://localhost:1234',
+      'http://movies.com'
+    ]
+    if (ACCEPTED_ORIGINS.includes(origin)) {
+      return callback(null, true)
+    }
+    if (!origin) {
+      return callback(null, true)
+    }
+    return callback(new Error('Not allowed by CORS'))
+  }
+})) // por defecto pone todo con *
+// es decir que permite todo tipo de rutas
+
+app.disable('x-powered-by') // desabilitar el header X-Powered-By: Express
+
+app.get('/', (req, res) => {
+  res.json({ message: 'hola mundo' })
+})
+
+// metodos normales: GET/HEAD/POST -> CORS PRE-Filght
+// metodos complejps: PUT/PATCH/DELET -> OPTIONS
+
+// todos los recursos que sean MOVIES se identifica con /movies
+app.get('/movies', (req, res) => {
+  const { genre } = req.query
+
+  if (genre) {
+    const fileredMovies = filter((movie) =>
+      movie.genre.some((g) => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+
+    return res.json(fileredMovies)
+  }
+  res.json(movies)
+})
+
+app.post('/movies', (req, res) => {
+  console.log('error: ', req.body)
+  const result = validateMovie(req.body)
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+  // crypto crea ids unicas
+  // en base de datos
+  const newMovie = {
+    id: randomUUID(),
+    ...result.data
+  }
+  // esto no es REST, porque estamos guardando el estado de la aplicacion en memoria
+  push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'movie not found' })
+  }
+  splice(movieIndex, 1)
+  return res.json({ message: 'movie deleted' })
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+  if (!result.success) return res.status(404).json({ message: JSON.parse(result.error.message) })
+
+  const { id } = req.params
+
+  const movieIndex = findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'movie not found' })
+  }
+
+  const updateMovie = { ...movies[movieIndex], ...result.data }
+
+  movies[movieIndex] = updateMovie
+
+  res.status(202).json(updateMovie)
+})
+
+app.get('/movies/:id', (req, res) => {
+  // path ro regex
+  const { id } = req.params
+  const movie = find((movie) => movie.id === id)
+  if (movie) return res.json(movie)
+
+  res.status(404).json({ message: 'movie not found' })
+})
+
+const PORT = process.env.PORT ?? 1234
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on port http://localhost:${PORT}`)
+})
