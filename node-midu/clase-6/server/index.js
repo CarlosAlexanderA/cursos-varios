3f9ed89ca11ab0f29fb74a7220d6803a68a47b64
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-02-19 21:00:01 -0500

    created db and show messages with users , scrolls

--- node-midu/clase-6/server/index.js
+++ node-midu/clase-6/server/index.js
@@ -1,9 +1,13 @@
 import express from 'express'
 import logger from 'morgan'
+import dotenv from 'dotenv'
+import { createClient } from '@libsql/client'
 
-import {Server} from 'socket.io'
-import {createServer} from 'node:http'
-import { createClient } from '@libsql/client/.'
+import { Server } from 'socket.io'
+import { createServer } from 'node:http'
+
+dotenv.config()
+
 
 
 const port = process.env.PORT ?? 3000
@@ -15,22 +19,54 @@ const io = new Server(server, {
 })
 
 const db= createClient({
-  url:"libsql://open-omega-red-carlosalexandera.turso.io",
+  url:"libsql://elegant-hulkling-carlosalexandera.turso.io",
   authToken: process.env.DB_TOKEN
 })
 
-await db.execute()
+await db.execute(`
+  CREATE TABLE IF NOT EXISTS messages (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    content TEXT,
+    user TEXT
+  )
+`)
 
-io.on('connection',(socket)=>{
+io.on('connection',async (socket)=>{
   console.log('a user has connected');
 
   socket.on('disconnect', ()=>{
     console.log('an user has disconnected');
   })
 
-  socket.on('chat message', msg => {
-    io.emit('chat message', msg)
+  socket.on('chat message', async (msg) => {
+
+    let result
+    const username = socket.handshake.auth.username ?? 'anonymous'
+    try {
+      result = await db.execute({
+        sql: 'INSERT INTO messages (content, user) VALUES (:message, :username)',
+        args: { message: msg , username} //! o { msg }
+      })
+    } catch (e) {
+      
+    }
+    io.emit('chat message', msg, result.lastInsertRowid.toString(), username)
   })
+  if(!socket.recovered){
+    try {
+      const results = await db.execute({
+        sql: 'SELECT id, content, user FROM messages WHERE id > ? ',
+        args: [socket.handshake.auth.serverOffset ?? 0]
+      })
+
+      results.rows.forEach(row=>{
+        socket.emit('chat message', row.content, row.id.toString(), row.user)
+      })
+
+    } catch (e) {
+      console.log(e);
+    }
+  }
 })
 
 app.use(logger('dev'))
