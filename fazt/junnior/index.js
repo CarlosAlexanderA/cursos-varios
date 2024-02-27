4f994c6e312daf449b676e7c128180f4324653e8
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-02-26 21:10:22 -0500

    sigueindo curso de fazt express

---
+++ fazt/junnior/index.js
@@ -0,0 +1,77 @@
+import express from 'express'
+
+const PORT = process.env.PORT || 3030
+const app = express()
+
+// para entender el texto del lado del cliente -> servidor
+app.use(express.text())
+
+app.use(express.json())
+
+// funciona con todos los metodos
+// GET, POST, PUT, ...
+app.all('/info', (req, res) => {
+  res.send('server info')
+})
+
+app.get('/', (req, res) => {
+  res.sendFile('./static/index.html', {
+    root: './'
+  })
+})
+
+app.get('/products', (req, res) => {
+  res.send('products')
+})
+// parametros en la url
+app.get('/hello/:user', (req, res) => {
+  console.log(req.params)
+  res.send(`Hello ${'asd'}`)
+})
+
+app.get('/add/:x/:Y', (req, res) => {
+  const { x, y } = req.params
+  res.send(`result: ${parseInt(x) + parent(y)}`)
+})
+
+app.get('/users/:username/:photo', (req, res) => {
+  if (req.params.username === 'javascript') {
+    return res.sendFile('./javascript.png', {
+      root: './'
+    })
+  }
+  res.send('el usuario no tiene acceso')
+})
+
+// queryes en la url -> http://...?x=ads
+app.get('/search', (req, res) => {
+  if (req.query.q === 'javascript') {
+    res.send('lista de libros de javascript')
+  } else {
+    res.send('page normal')
+  }
+})
+app.get('/user', (req, res) => {
+  res.json({
+    name: 'none',
+    lastname: 'jssol',
+    age: 40,
+    points: [1, 2, 3],
+    address: {
+      city: 'new york',
+      street: 'some street 123'
+    }
+  })
+})
+
+app.post('/user', (req, res) => {
+  console.log(req.body)
+  res.send('Nuevo usuario creado')
+})
+
+app.use((req, res) => {
+  res.status(404).send('pagina no encontrada')
+})
+app.listen(PORT, () => {
+  console.log(`server running in http://localhost:${PORT}`)
+})
