18f8b0738a93d3553792a9b7be5f5b7e4ca32ecd
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-03-06 23:45:45 -0500

    save music with multer

--- appdelante/music api/index.js
+++ appdelante/music api/index.js
@@ -2,10 +2,23 @@ import express from 'express'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import mediaserver from 'mediaserver'
+import multer from 'multer'
+import fs from 'node:fs'
 
 const PORT = process.env.PORT || 3050
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
+
+const optionsMulter = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, 'songs'))
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname)
+  }
+})
+
+const upload = multer({ storage: optionsMulter })
 
 const app = express()
 app.set('appname', 'api music')
@@ -24,6 +37,22 @@ app.get('/songs/:name', (req, res) => {
   const song = path.join(__dirname, 'songs', req.params.name)
   console.log(song)
   mediaserver.pipe(req, res, song)
+})
+
+app.post('/songs', upload.single('song'), (req, res) => {
+  const fileSongs = path.join(__dirname, 'songs.json')
+  const name = req.file.originalname
+  fs.readFile(fileSongs, 'utf-8', (err, file) => {
+    if (err) throw err
+
+    const songs = JSON.parse(file)
+    songs.push({ name })
+    fs.writeFile(fileSongs, JSON.stringify(songs), (err) => {
+      if (err) throw err
+
+      res.sendFile('./index.html', { root: './' })
+    })
+  })
 })
 
 app.listen(PORT, () => {
--- appdelante/music api/public/js/main.js
+++ appdelante/music api/public/js/main.js
@@ -1,3 +1,5 @@
+const audio = document.getElementById('audio')
+audio.volume = 0.1
 console.log('iam in the browser')
 const loadSongs = async () => {
   try {
@@ -7,11 +9,19 @@ const loadSongs = async () => {
     const list = document.getElementById('list')
 
     json.forEach(item => {
-      list.innerHTML += `<li>${item.name}</li>`
+      const li = document.createElement('li')
+      li.textContent += item.name
+      li.addEventListener('click', () => { play(item) })
+      list.appendChild(li)
     })
   } catch (error) {
     console.log(error)
   }
+}
+const play = (e) => {
+  audio.pause()
+  audio.src = '/songs/' + e.name
+  audio.play()
 }
 
 loadSongs()
