70e2ee22f68e92063f72b3d75a4a9a0dfd291bdf
CarlosAlexanderA <acerohuarahuara7551@gmail.com>
2024-04-26 21:26:37 -0500

    start project setup, app navigate and styling dark mode

---
+++ react-native/music-player/src/app/(tabs)/_layout.tsx
@@ -0,0 +1,80 @@
+import {colors, fontSize} from '@/constants/tokens';
+import {
+  FontAwesome,
+  FontAwesome6,
+  Ionicons,
+  MaterialCommunityIcons,
+} from '@expo/vector-icons';
+import {BlurView} from 'expo-blur';
+import {Tabs} from 'expo-router';
+import {StyleSheet} from 'react-native';
+export default function TabasNavigation() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {fontSize: fontSize.xs, fontWeight: '500'},
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          borderTopLeftRadius: 28,
+          borderTopRightRadius: 28,
+          borderWidth: 0,
+          paddingTop: 8,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={20}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: 'hidden',
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        ),
+      }}
+    >
+      <Tabs.Screen
+        name="favorites"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({color}) => (
+            <FontAwesome name="heart" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="playlists"
+        options={{
+          title: 'Playlist',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons
+              name="playlist-play"
+              size={28}
+              color={color}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="(songs)"
+        options={{
+          title: 'Songs',
+          tabBarIcon: ({color}) => (
+            <Ionicons name="musical-notes-sharp" size={24} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="artists"
+        options={{
+          title: 'Artists',
+          tabBarIcon: ({color}) => (
+            <FontAwesome6 name="users-line" size={20} color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
